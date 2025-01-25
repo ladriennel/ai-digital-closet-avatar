272d570fa4ced6ffcf0eed3cfb654ddb048c01cb
Bryan Hong <bzh.texas@gmail.com>
2025-01-24 21:15:00 -0800

    push for change

--- backend/app/models.py
+++ backend/app/models.py
@@ -6,7 +6,6 @@ from PIL import Image
 from ultralytics import YOLO
 import os
 import uuid
-from pymatting import estimate_alpha_cf
 
 class ClothingClassifier:
     def __init__(self):
@@ -188,10 +187,7 @@ class ClothingClassifier:
         
         return rgba
 
-
-
-
-        
+   
 classifier = ClothingClassifier()
 
         
